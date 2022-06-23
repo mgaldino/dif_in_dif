b5a4b20d31511e30d97060e020f4407a2e5bada2
mgaldino <mgaldino@gmail.com>
2022-06-23 13:45:00 -0300

    Limpei o código um pouco, mas ainda preciso comentar

--- sim_DiD.R
+++ sim_DiD.R
@@ -3,6 +3,7 @@ library(fixest)
 library(tidyverse)
 library(ggplot2)
 library(tidyr)
+library(bacondecomp)
 
 ## Um tratamento apenas, efeito homogêneo no tempo
 # experimento:
@@ -83,149 +84,6 @@ summary(reg)
 reg2 <- feols(y ~ x | grupos + periodos, data=df1)
 summary(reg2)
 
-### 
-## três rodadas
-## a cada rodada, indivíduo pode estar em um dos trÊs grupos
-## A é controle, B é tratamento 1, C é tratamento 2
-## efeito homogêneo no tempo de cada tratamento
-n <- 1000
-
-df <- expand.grid(
-  # periodos = c(0,1,2),
-  id = 1:n,
-  grupos = c("A", "B", "C"),
-  intercept = rep(2,n),
-  stringsAsFactors = F
-) %>%
-  group_by(id) %>%
-  slice_sample(n=1) %>%
-  as_tibble() %>%
-  mutate(treatment_0 = as.numeric(grupos == "A"),
-         treatment_1 = as.numeric(grupos == "B"),
-         treatment_2 = as.numeric(grupos %in% c("B", "C"))) %>%
-  ungroup() %>%
-  # group_by(id_unit, periodos) %>%
-  # mutate(grupos = max(grupos))
-  mutate(erro0 = rnorm(n(), 3),
-         erro1 = rnorm(n(), 3),
-         erro2 = rnorm(n(), 3),
-         grupo_b = as.numeric(grupos == "B"),
-         grupo_c = as.numeric(grupos == "C"),
-         delta_b = 8,
-         delta_c = 10,
-         delta_1 = 12,
-         delta_2 = 15,
-         alpha_group_B = delta_b*grupo_b,
-         alpha_group_C = delta_c*grupo_c,
-         delta_treatment = 20,
-         y0 = intercept + alpha_group_B + alpha_group_C + erro0,
-         y1 = intercept + delta_1 + alpha_group_B + alpha_group_C + delta_treatment*treatment_1 + erro1,
-         y2 = intercept + delta_1 + delta_2 + alpha_group_B + alpha_group_C + 
-           delta_treatment*treatment_1 +  delta_treatment*treatment_2 + erro2)
-
-
-
-df1 <- df %>%
-  select(id, grupos, y0, y1, y2, treatment_0, treatment_1, treatment_2) %>%
-  pivot_longer(cols = c(treatment_0, treatment_1, treatment_2),
-               names_to = "periodo",
-               values_to = "tratamento",
-               names_prefix = "treatment_") %>%
-  pivot_longer(cols = c(y0, y1, y2),
-               names_to = "periodo1",
-               values_to = "y",
-               names_prefix = "y") %>%
-  group_by(id, grupos, periodo1 ) %>%
-  summarise(y = max(y)) %>%
-  mutate(periodo1 = as.numeric(periodo1),
-         periodo2 = as.numeric(periodo1 > 0), 
-         periodo3 = periodo1 - 1,
-         periodo3 = ifelse(periodo3 < 0, 0, periodo3),
-         x = case_when(grupos == "B" ~ periodo2,
-                       grupos == "C" ~ periodo2*periodo3,
-                       TRUE ~ 0)) %>%
-  select(-c(periodo2, periodo3)) %>%
-  rename(periodos =periodo1)
-
-
-reg <- lm(y ~ as.factor(periodos) + as.factor(grupos) + x, data=df1)
-summary(reg)
-
-reg2 <- feols(y ~ x | grupos + periodos, data=df1)
-summary(reg2)
-
-
-# agora, efeito de tratamento heterogêneo
-
-
-n <- 1000
-
-df <- expand.grid(
-  # periodos = c(0,1,2),
-  id = 1:n,
-  grupos = c("A", "B", "C"),
-  intercept = rep(2,n),
-  stringsAsFactors = F
-) %>%
-  group_by(id) %>%
-  slice_sample(n=1) %>%
-  as_tibble() %>%
-  mutate(treatment_0 = as.numeric(grupos == "A"),
-         treatment_1 = as.numeric(grupos == "B"),
-         treatment_2 = as.numeric(grupos %in% c("B", "C"))) %>%
-  ungroup() %>%
-  # group_by(id_unit, periodos) %>%
-  # mutate(grupos = max(grupos))
-  mutate(erro0 = rnorm(n(), 3),
-         erro1 = rnorm(n(), 3),
-         erro2 = rnorm(n(), 3),
-         grupo_b = as.numeric(grupos == "B"),
-         grupo_c = as.numeric(grupos == "C"),
-         delta_b = 8,
-         delta_c = 10,
-         delta_1 = 12,
-         delta_2 = 15,
-         alpha_group_B = delta_b*grupo_b,
-         alpha_group_C = delta_c*grupo_c,
-         delta_treatment1 = 30,
-         delta_treatment2 = 10,
-         y0 = intercept + alpha_group_B + alpha_group_C + erro0,
-         y1 = intercept + delta_1 + alpha_group_B + alpha_group_C + delta_treatment1*treatment_1 + erro1,
-         y2 = intercept + delta_1 + delta_2 + alpha_group_B + alpha_group_C + 
-           delta_treatment1*treatment_1 +  delta_treatment2*treatment_2 + erro2)
-
-
-
-df1 <- df %>%
-  select(id, grupos, y0, y1, y2, treatment_0, treatment_1, treatment_2) %>%
-  pivot_longer(cols = c(treatment_0, treatment_1, treatment_2),
-               names_to = "periodo",
-               values_to = "tratamento",
-               names_prefix = "treatment_") %>%
-  pivot_longer(cols = c(y0, y1, y2),
-               names_to = "periodo1",
-               values_to = "y",
-               names_prefix = "y") %>%
-  group_by(id, grupos, periodo1 ) %>%
-  summarise(y = max(y)) %>%
-  mutate(periodo1 = as.numeric(periodo1),
-         periodo2 = as.numeric(periodo1 > 0), 
-         periodo3 = periodo1 - 1,
-         periodo3 = ifelse(periodo3 < 0, 0, periodo3),
-         x = case_when(grupos == "B" ~ periodo2,
-                       grupos == "C" ~ periodo2*periodo3,
-                       TRUE ~ 0)) %>%
-  select(-c(periodo2, periodo3)) %>%
-  rename(periodos =periodo1)
-
-reg <- lm(y ~ as.factor(periodos) + as.factor(grupos) + x, data=df1)
-summary(reg)
-
-reg2 <- feols(y ~ x | grupos + periodos, data=df1)
-summary(reg2)
-
-
-
 # agora, efeito de tratamento heterogêneo, três tratamentos, n períodos = 30
 
 
@@ -274,3 +132,78 @@ summary(reg)
 
 reg2 <- feols(y ~ timing | grupos + time , data=df)
 summary(reg2)
+
+df_bacon <- bacon(y ~ timing,
+                  data = df,
+                  id_var = "id",
+                  time_var = "time")
+
+#>                       type  weight  avg_est
+#> 1 Earlier vs Later Treated 0.05976 -0.00554
+#> 2 Later vs Earlier Treated 0.03190  0.07032
+#> 3     Treated vs Untreated 0.90834  0.08796
+coef_bacon <- sum(df_bacon$estimate * df_bacon$weight)
+print(paste("Weighted sum of decomposition =", round(coef_bacon, 4)))
+#> [1] "Weighted sum of decomposition = 0.0818"
+#> 
+#> 
+## tratamento variando no tempo
+
+
+
+n <- 900
+t <- 30
+grupos <- rep(c("A", "B", "C"), n/3)
+k <- t/3
+l <- t - k
+
+df <- expand.grid(
+  # periodos = c(0,1,2),
+  id = 1:n,
+  time = 1:t,
+  # grupos = c("A", "B", "C"),
+  # intercept = rep(2,n),
+  stringsAsFactors = F
+) %>%
+  as_tibble() %>%
+  mutate(intercept = 2,
+         grupos = rep(grupos, t),
+         treatment_0 = as.numeric(grupos == "A"),
+         treatment_1 = as.numeric(grupos == "B"),
+         treatment_2 = as.numeric(grupos == "C"),
+         timing = ifelse(grupos == "B" & time > k, 1,
+                         ifelse(grupos == "C" & time > l, 1, 0)),
+         erro = rnorm(n(), 3),
+         grupo_b = as.numeric(grupos == "B"),
+         grupo_c = as.numeric(grupos == "C"),
+         delta_b = 15,
+         delta_c = 5,
+         delta_time = .5, # efeito do tempo
+         alpha_group_B = delta_b*grupo_b,
+         alpha_group_C = delta_c*grupo_c,
+         delta_treatment = 2*time,
+         efeito_tempo = delta_time*time,
+         y = intercept + delta_time*time + alpha_group_B*treatment_1 + alpha_group_C*treatment_2 + 
+           delta_treatment*timing*treatment_1 +  delta_treatment*timing*treatment_2 + erro)
+
+df %>%
+  ggplot(aes(y=y, x=time, colour=grupos)) +geom_point() #+geom_smooth(method = "lm")
+
+reg <- lm(y ~ as.factor(time) + as.factor(grupos) + timing, data=df)
+summary(reg)
+
+reg2 <- feols(y ~ timing | grupos + time , data=df)
+summary(reg2)
+
+df_bacon <- bacon(y ~ timing,
+                  data = df,
+                  id_var = "id",
+                  time_var = "time")
+
+#>                       type  weight  avg_est
+#> 1 Earlier vs Later Treated 0.05976 -0.00554
+#> 2 Later vs Earlier Treated 0.03190  0.07032
+#> 3     Treated vs Untreated 0.90834  0.08796
+coef_bacon <- sum(df_bacon$estimate * df_bacon$weight)
+print(paste("Weighted sum of decomposition =", round(coef_bacon, 4)))
+
